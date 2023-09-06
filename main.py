d63d951a926f3f2640edb21a13593c2bc58eb4fe
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-06 13:14:18 +0200

    Övning

---
+++ main.py
@@ -0,0 +1,9 @@
+# Läs in två tal från användaren
+tal1 = float(input("Ange det första talet: "))
+tal2 = float(input("Ange det andra talet: "))
+
+# Beräkna summan
+summa = tal1 + tal2
+
+# Visa resultatet
+print(f"Summan av {tal1} och {tal2} är {summa}")
